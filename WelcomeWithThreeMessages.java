4a0138be5fdc8f316d5970198fa04d178d9e5abf
amikalperkins <a.mikalperkins@gmail.com>
2021-08-17 14:09:45 -0700

    Three_Messages

--- WelcomeWithThreeMessages.java
+++ WelcomeWithThreeMessages.java
@@ -1,5 +1,7 @@
 public class WelcomeWithThreeMessages {
     public static void main(String[] args){
         System.out.println("Programming is fun!");
+        System.out.println("Git is fun!");
+        System.out.println("Life is fun!");
     }
 }
