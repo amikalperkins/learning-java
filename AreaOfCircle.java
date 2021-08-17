1c386f8e28c60fd889cd9e28cfc3a7dffe2e8b78
amikalperkins <a.mikalperkins@gmail.com>
2021-08-17 16:23:16 -0700

    areaofcircle

---
+++ AreaOfCircle.java
@@ -0,0 +1,14 @@
+public class AreaOfCircle {
+    public static void main(String[] args){
+        double radius;
+        double area;
+        radius = 5;
+        // Step 1: Read in radius
+        
+        // Step 2: Compute Area
+        area = (radius * radius) * 3.14159;
+
+        // Step 3: Display the Area
+        System.out.println("The area for the circle of raius " + radius + " is " + area);
+    }
+}
