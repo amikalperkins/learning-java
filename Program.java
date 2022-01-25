8ae6845cb53244a5085cf5b041ad9c5654840b52
amikalperkins <a.mikalperkins@gmail.com>
2022-01-24 18:22:36 -0800

    updated temperature class

--- Program.java
+++ Program.java
@@ -1,25 +1,76 @@
-// Temperature Instantiation 
-// Driver Testing : see pg 423 
-// Create a Driver Program that tests all methods
-// add Character.toUpperCase() on input to make all 
-// chars UpperCase
-// when testing driver program think of your object
-// creation and how to pass those references
-// tests all methods, invoke each constructor, to include at least
-// one true and one false case for each comparison method
-// and to test at least the following temperature pairs
-// for equality.
+/*
+ * Author: Aaron Perkins
+   Email : perkina6@wwu.edu
+   Last Changed: January 24th, 2022 
+   Project description:
+   Temperature Instantiation 
+   Create a Driver Program that tests all 
+   methods, invoke each constructor, include 
+   at least one true and one false case for each 
+   comparison method and to test for equality, 
+   less than, greater than
+*/
+
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Temperature hot = new Temperature(-40f,'f');
-		Temperature samesies = new Temperature(-40f, 'c');
+		Scanner keyboard = new Scanner(System.in);
+		String response;
+		float temperature;
+		char scale;
+		
+	
+		do {
+			//Input from keyboard values to instantiate temperature
+			//class
+			System.out.println("Enter a temperature value: ");
+			temperature = keyboard.nextFloat();
+			System.out.println("Enter a temperature scale (C or F): ");
+			scale = Character.toUpperCase(keyboard.next().charAt(0));
+			Temperature temp1 = new Temperature(temperature, scale);
+			System.out.println("Enter a temperature value: ");
+			temperature = keyboard.nextFloat();
+			System.out.println("Enter a temperature scale (C or F): ");
+			scale = Character.toUpperCase(keyboard.next().charAt(0));
+			Temperature temp2 = new Temperature(temperature, scale);
+			// Test Accessors : getCelsius() getFahrenheit
+			System.out.println("Temperature in Celsius " + temp2.getTempCelsius());
+			System.out.println("Temperature in Fahrenheit " + temp1.getTempFahrenheit());
+			//Equality, Greater Than, and Less Than Check
+			System.out.println("Are the temperatures equal? " + temp1.equalityCheckOfTemps(temp2));
+			System.out.println("Is " + temp1.getTemperature() + temp1.getScale() + " > " + temp2.getTemperature() + temp2.getScale() 
+			+ " : " + temp1.greaterThanCheckOfTemps(temp2));
+			System.out.println("Is " + temp1.getTemperature() + temp1.getScale() + " < " + temp2.getTemperature() + temp2.getScale() 
+			+ " : " + temp1.lessThanCheckOfTemps(temp2));
+			//Test Mutators : setNumberDegrees(), setScale(), setScaleAndDegrees()
+			temp2.setNumberDegrees(25);
+			System.out.println("Set new degrees: " + temp2.getTemperature());
+			temp2.setScale('F');
+			System.out.println("Set new scale: " + temp2.getScale());
+			temp2.setScaleAndDegrees(30, 'C');
+			System.out.println("Degrees and scale changed: " + temp2.getTemperature() + temp2.getScale());
+			//Constructor Testing
+			Temperature defaultConstructor = new Temperature();
+			System.out.println("Default constructor temp: " + defaultConstructor.getTemperature() + " Default Scale: " + defaultConstructor.getScale());
+			Temperature degreesConstructor = new Temperature(10);
+			System.out.println("Degrees Constructor temp: " + degreesConstructor.getTemperature());
+			Temperature degreesScaleConstructor = new Temperature(15, 'F');
+			System.out.println("Degrees and Scale Constructor: " + degreesScaleConstructor.getTemperature() + degreesScaleConstructor.getScale());
+			System.out.println("Test again?");
+			response = keyboard.next();
+		
+		} while(response.equalsIgnoreCase("yes"));
+		System.out.println("End of test");
+		
+		
+		keyboard.close();
+		
 
-		System.out.println(hot.equalityCheckOfTemps(samesies));
-		System.out.println(hot.greaterThanCheckOfTemps(samesies));	
-		System.out.println(hot.lessThanCheckOfTemps(samesies));
-		}
+		
 
+	}
 }
+
--- Temperature.java
+++ Temperature.java
@@ -1,109 +1,104 @@
-
-
 /*
  * Author: Aaron Perkins
    Email : perkina6@wwu.edu
-   Last Changed: January 16th, 2022 
+   Last Changed: January 24th, 2022 
    Project description:
    Create class Temperature that represents 
    temperature in degrees in both Celsius and 
    Fahrenheit. Test for equality, less than and
    greater than of temperatures.
    
-   
-   Overloading : see pg 430
    */
-	
+  import java.math.BigDecimal;
+  import java.math.RoundingMode;
+  
   public class Temperature {
-	
-
-	private float temperature;
-	private char scale;
-	private float convertedTemp;
-	private float tempCelsius;
-	private float tempFahrenheit;
-	
-	// Constructors
-	// if no values passed assume default constructor
-	public  Temperature() {
-		temperature = 0.0f;
-		scale = 'C';
-	}
-	public  Temperature(float degrees, char scaleTemp) {
-		this(); //invokes default constructor if one or the other parameter is not passed
-		temperature = degrees;
-		scale = scaleTemp;
-		//scale = Character.toUpperCase(scaleTemp);	
-		//instead of Character.toUpperCase() on all of these I just put it in the driver
-		// function and restrict on keyboard input
-	}
-	public Temperature(float degrees) {
-		this();
-		temperature = degrees;
-	}
-	public Temperature(char scaleTemp) {
-		this();
-		scale = scaleTemp;
-	}
-
-	// accessors 
-	public float getTemperature() {
-		return temperature;
-	}
-	
-	public char getScale() {
-		return scale;
-	}
-	
-	// return a formatted value? .1 
-	public float getTempCelsius() {
-		tempCelsius = (5.0f*(getTemperature() - 32.0f))/9.0f;
-		return tempCelsius;
-	}
-	public float getTempFahrenheit() {
-		tempFahrenheit = (9.0f*(getTemperature()/5.0f) + 32.0f);
-		return tempFahrenheit;
-	}
-	
-	public void setNumberDegrees(float degrees) {
-		temperature = degrees;
-	}
-	
-	public void setScale(char scaleTemp) {
-		scale = scaleTemp;
-	}
-	public void setScaleAndDegrees(float degrees, char scaleTemp) {
-		temperature = degrees;
-		scale = scaleTemp;
-	}
-
-	
-	// testing Equality of two temperature objects
-	public boolean equalityCheckOfTemps(Temperature otherTempClass) {
-		//call private conversion scale 
-		scaleConversion(otherTempClass);
-		return this.getTemperature() == otherTempClass.convertedTemp ? true : false;
-	}
-	//testing Greater than of two temperature objects
-	public	boolean greaterThanCheckOfTemps(Temperature otherTempClass) {
-		return this.getTemperature() > otherTempClass.convertedTemp  ? true : false;
-	}
-	//testing Less than of two temperature objects
-	public boolean lessThanCheckOfTemps(Temperature otherTempClass) {
-		return this.getTemperature() < otherTempClass.convertedTemp ? true : false;	
-		
-	}
-	// helper method to convert to same scale and then pass along
-	// converted temperature to test for ==, > and <
-	private void scaleConversion(Temperature otherTempClass) {
-		if(this.scale == 'C' && otherTempClass.scale != 'C'){
-			otherTempClass.convertedTemp = otherTempClass.getTempCelsius();
-		} else if(this.scale == 'F' && otherTempClass.scale != 'F') {
-			otherTempClass.convertedTemp = otherTempClass.getTempFahrenheit();
-		} else if(this.scale == 'C' && otherTempClass.scale == 'C') {
-			otherTempClass.convertedTemp = otherTempClass.getTemperature();
-		} else {
-			otherTempClass.convertedTemp = otherTempClass.getTemperature();
-		}
-	}
-}
+	  
+	  
+	  private float temperature;
+	  private char scale;
+	  private float convertedTemp;
+	  
+	  // Constructors
+	  // if no values passed assume default constructor
+	  public  Temperature() {
+		  temperature = 0.0f;
+		  scale = 'C';
+	  }
+	  public  Temperature(float degrees, char scaleTemp) {
+		  this(); //invokes default constructor if one or the other parameter is not passed
+		  temperature = degrees;
+		  scale = scaleTemp;
+	  }
+	  public Temperature(float degrees) {
+		  this();
+		  temperature = degrees;
+	  }
+	  public Temperature(char scaleTemp) {
+		  this();
+		  scale = scaleTemp;
+	  }
+  
+	  // accessors 
+	  public float getTemperature() {
+		  return temperature;
+	  }
+	  
+	  public char getScale() {
+		  return scale;
+	  }
+	  
+	  // return a formatted value? .1 
+	  public float getTempCelsius() {
+		  BigDecimal tempCelsius = new BigDecimal((5.0f*(getTemperature() - 32.0f))/9.0f).setScale(1, RoundingMode.CEILING);
+		  return tempCelsius.floatValue();
+	  }
+	  public float getTempFahrenheit() {
+		  BigDecimal tempFahrenheit = new BigDecimal((9.0f*(getTemperature()/5.0f) + 32.0f)).setScale(1,RoundingMode.CEILING);
+		  return tempFahrenheit.floatValue();
+	  }
+	  
+	  public void setNumberDegrees(float degrees) {
+		  temperature = degrees;
+	  }
+	  
+	  public void setScale(char scaleTemp) {
+		  scale = scaleTemp;
+	  }
+	  public void setScaleAndDegrees(float degrees, char scaleTemp) {
+		  temperature = degrees;
+		  scale = scaleTemp;
+	  }
+  
+	  
+	  // testing Equality of two temperature objects
+	  public boolean equalityCheckOfTemps(Temperature otherTempClass) {
+		  //call private conversion scale 
+		  scaleConversion(otherTempClass);
+		  return this.getTemperature() == otherTempClass.convertedTemp ? true : false;
+	  }
+	  //testing Greater than of two temperature objects
+	  public	boolean greaterThanCheckOfTemps(Temperature otherTempClass) {
+		  return this.getTemperature() > otherTempClass.convertedTemp  ? true : false;
+	  }
+	  //testing Less than of two temperature objects
+	  public boolean lessThanCheckOfTemps(Temperature otherTempClass) {
+		  return this.getTemperature() < otherTempClass.convertedTemp ? true : false;	
+		  
+	  }
+	  // helper method to convert to same scale and then pass along
+	  // converted temperature to test for ==, > and <
+	  // pass the parameters instead of the entire class
+	  // for memory usage don't pass the entire object just the parameter you need
+	  private void scaleConversion(Temperature otherTempClass) {
+		  if(this.scale == 'C' && otherTempClass.scale != 'C'){
+			  otherTempClass.convertedTemp = otherTempClass.getTempCelsius();
+		  } else if(this.scale == 'F' && otherTempClass.scale != 'F') {
+			  otherTempClass.convertedTemp = otherTempClass.getTempFahrenheit();
+		  } else if(this.scale == 'C' && otherTempClass.scale == 'C') {
+			  otherTempClass.convertedTemp = otherTempClass.getTemperature();
+		  } else {
+			  otherTempClass.convertedTemp = otherTempClass.getTemperature();
+		  }
+	  }
+  }
